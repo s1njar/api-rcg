6ba43d0dcba531d0b0e7ea99a84834bfaa7ef557
s1njar <s1njar.mail@gmail.com>
2020-02-13 23:53:40 +0100

    [TASK] Removed life generator

--- Modules/Generator/Services/Generator/CardGeneratorService.php
+++ Modules/Generator/Services/Generator/CardGeneratorService.php
@@ -7,10 +7,10 @@
 
 namespace Modules\Generator\Services\Generator;
 
+use Exception;
 use Modules\Generator\Model\CardGeneratorModel;
 use Modules\Generator\Services\Generator\Attributes\CardTypeGeneratorService;
 use Modules\Generator\Services\Generator\Attributes\CategoryGeneratorService;
-use Modules\Generator\Services\Generator\Attributes\LifeGeneratorService;
 use Modules\Generator\Services\Generator\Attributes\NameGeneratorService;
 use Modules\Generator\Services\Generator\Attributes\PictureGeneratorService;
 use Modules\Generator\Services\Generator\Attributes\RarityGeneratorService;
@@ -40,10 +40,6 @@ class CardGeneratorService
      * @var CardTypeGeneratorService
      */
     private $cardTypeGeneratorService;
-    /**
-     * @var LifeGeneratorService
-     */
-    private $lifeGeneratorService;
 
     /**
      * CardGeneratorService constructor.
@@ -53,30 +49,27 @@ class CardGeneratorService
      * @param PictureGeneratorService $pictureGeneratorService
      * @param RarityGeneratorService $rarityGeneratorService
      * @param CardTypeGeneratorService $cardTypeGeneratorService
-     * @param LifeGeneratorService $lifeGeneratorService
      */
     public function __construct(
         CategoryGeneratorService $categoryGeneratorService,
         NameGeneratorService $nameGeneratorService,
         PictureGeneratorService $pictureGeneratorService,
         RarityGeneratorService $rarityGeneratorService,
-        CardTypeGeneratorService $cardTypeGeneratorService,
-        LifeGeneratorService $lifeGeneratorService
+        CardTypeGeneratorService $cardTypeGeneratorService
     ) {
         $this->categoryGeneratorService = $categoryGeneratorService;
         $this->nameGeneratorService = $nameGeneratorService;
         $this->pictureGeneratorService = $pictureGeneratorService;
         $this->rarityGeneratorService = $rarityGeneratorService;
         $this->cardTypeGeneratorService = $cardTypeGeneratorService;
-        $this->lifeGeneratorService = $lifeGeneratorService;
     }
-
+r
     /**
      * Responsible to create attributes in correct queue.
      *
      * @param CardGeneratorModel $cardGeneratorModel
      * @return CardGeneratorModel
-     * @throws \Exception
+     * @throws Exception
      */
     public function execute(CardGeneratorModel $cardGeneratorModel): CardGeneratorModel
     {
@@ -85,7 +78,6 @@ class CardGeneratorService
         $this->pictureGeneratorService->execute($cardGeneratorModel);
         $this->rarityGeneratorService->execute($cardGeneratorModel);
         $this->cardTypeGeneratorService->execute($cardGeneratorModel);
-        $this->lifeGeneratorService->execute($cardGeneratorModel);
 
         return $cardGeneratorModel;
     }
